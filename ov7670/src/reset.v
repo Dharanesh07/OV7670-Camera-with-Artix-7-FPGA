fdd554e6ca71f594e34ba0c4e78c29af4120671e
Dharaneshwar Sasidharan <78021498+Dharanesh07@users.noreply.github.com>
2026-02-26 19:06:37 +0100

    Update reset.v

--- ov7670/src/reset.v
+++ ov7670/src/reset.v
@@ -1,9 +1,9 @@
-module #(
+module reset #(
     parameter RESET_CYCLES = 32'd100000
-) reset(
-    input i_clk,
-    wire o_rstn,
-    wire o_rst_done
+) (
+    input i_rst_clk,
+    output o_rstn,
+    output o_rst_done
 );
 
   reg rstn = 0;
@@ -12,10 +12,10 @@ module #(
 
   // 10 seconds at 50MHz
   // localparam RESET_CYCLES = 32'd500000000;
-  localparam RESET_CYCLES = 32'd100000;
+  // localparam RESET_CYCLES = 32'd100000;
 
   reg status;
-  always @(posedge sys_clk) begin
+  always @(posedge i_rst_clk) begin
     if ((r_rst_cycle < RESET_CYCLES) && (!rst_done)) begin
       r_rst_cycle <= r_rst_cycle + 1;
       rstn        <= 1'b0;
